0a55ea012a61c6aa15ae2f41ff19bd8b8b1962e0
JP <JPE011221122@gmail.com>
2023-11-01 04:28:15 -0700

    Cameras Plus added a reset yaw button

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/EngiNERDs_Control_FC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/EngiNERDs_Control_FC.java
@@ -96,7 +96,7 @@ public class EngiNERDs_Control_FC extends LinearOpMode {
             // This button choice was made so that it is hard to hit on accident,
             // it can be freely changed based on preference.
             // The equivalent button is start on Xbox-style controllers.
-            if (gamepad1.options) {
+            if (gamepad1.back) {
                 imu.resetYaw();
             }
         }
