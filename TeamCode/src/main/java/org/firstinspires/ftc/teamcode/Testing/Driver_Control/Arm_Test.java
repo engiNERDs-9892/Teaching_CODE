33600876dd6093332cbc646943e004ab6d2e886f
JP <JPE011221122@gmail.com>
2024-03-02 10:29:15 -0800

    Updating the project to fit the new robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Driver_Control/Arm_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Driver_Control/Arm_Test.java
@@ -3,40 +3,36 @@ package org.firstinspires.ftc.teamcode.Testing.Driver_Control;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
+import static org.firstinspires.ftc.teamcode.Tuning_Variables.EngiNERDs_Variables.FlippyFlip;
+import static org.firstinspires.ftc.teamcode.Tuning_Variables.EngiNERDs_Variables.FlooppyFloop;
+
+
 
 
 @Autonomous(group = "advanced")
 public class Arm_Test extends LinearOpMode {
-
-    public Servo servo0;
-    public Servo servo1;
-    public Servo servo2;
-    public Servo servo3;
-    public Servo servo4;
-    public Servo servo5;
-    public Servo servo6;
     @Override
     public void runOpMode() throws InterruptedException {
 
 
-        servo3 = hardwareMap.servo.get("servo3");
-        servo4 = hardwareMap.servo.get("servo4");
-        servo4.setDirection(Servo.Direction.REVERSE);
+        FlooppyFloop = hardwareMap.servo.get("FlooppyFloop"); // RIGHT SERVO
+        FlippyFlip = hardwareMap.servo.get("FlippyFlip"); // LEFT SERVO
+        FlippyFlip.setDirection(Servo.Direction.REVERSE); // LEFT SERVO
 
         // this initializes the camera (Not going into it tooo much but it initalizes the camera + hw map, and the pipline as well)
 
-        servo3.setPosition(0);
-        servo4.setPosition(0);
+        FlooppyFloop.setPosition(0);
+        FlippyFlip.setPosition(0);
         waitForStart();
 
         while (opModeIsActive()) {
         if (gamepad1.a){
-            servo3.setPosition(0);
-            servo4.setPosition(0);
+            FlooppyFloop.setPosition(0);
+            FlippyFlip.setPosition(0);
         }
         if (gamepad1.b){
-            servo3.setPosition(.25);
-            servo4.setPosition(.25);
+            FlooppyFloop.setPosition(.25);
+            FlippyFlip.setPosition(.25);
         }
 
         }
